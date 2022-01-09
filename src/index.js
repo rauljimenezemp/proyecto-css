ee301b8ded91dea8fdaff495a5ba47000fe6d921
Raúl JR <83794310+rauljimenezemp@users.noreply.github.com>
2022-01-08 22:10:43 -0600

    Acomode archivos

--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
